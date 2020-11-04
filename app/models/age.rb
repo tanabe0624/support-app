fbcfd9fd002530752fbe9a855496a5b37a33a0db
tanabe0624 <ryo.tnb.bn@gmail.com>
2020-11-04 15:21:00 +0900

    Update age.rb

--- app/models/age.rb
+++ app/models/age.rb
@@ -3,7 +3,7 @@ class Age < ActiveHash::Base
    { id: 1, name: '--' },
    { id: 2, name: '10代以下' },
    { id: 3, name: '20代' },
-   { id: 4, name: '3０代' },
+   { id: 4, name: '30代' },
    { id: 5, name: '40代' },
    { id: 6, name: '50代' },
    { id: 7, name: '60代' },
