0115ecf47244bd194a023da285c0cf6e8a022c4c
tanabe0624 <ryo.tnb.bn@gmail.com>
2020-11-04 15:20:54 +0900

    add active_hash

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :age
+  belongs_to_active_hash :category
+  belongs_to_active_hash :gender
+  belongs_to_active_hash :occupation
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
