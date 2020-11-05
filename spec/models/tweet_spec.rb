f240f98eb47821bdfcfd9eee4adce3ef55872710
tanabe0624 <ryo.tnb.bn@gmail.com>
2020-11-05 15:16:39 +0900

    tweet model 単体テストコード

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe Tweet, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      @tweet = FactoryBot.build(:tweet)
+    end
+
+    it 'title,text,category_idが存在していれば保存できること' do
+      expect(@tweet).to be_valid
+    end
+
+    it 'titleが空では保存できないこと' do
+      @tweet.title = nil
+      @tweet.valid?
+      expect(@tweet.errors.full_messages).to include("Title can't be blank")
+    end
+
+    it 'textが空では保存できないこと' do
+      @tweet.text = nil
+      @tweet.valid?
+      expect(@tweet.errors.full_messages).to include("Text can't be blank")
+    end
+
+    it 'category_idが空では保存できないこと' do
+      @tweet.category_id = nil
+      @tweet.valid?
+      expect(@tweet.errors.full_messages).to include("Category can't be blank")
+    end
+
+    it 'cateogry_idが1では保存できないこと' do
+      @tweet.category_id = 1
+      @tweet.valid?
+      expect(@tweet.errors.full_messages).to include('Category must be other than 1')
+    end
+
+    it 'userが紐付いていないと保存できないこと' do
+      @tweet.user = nil
+      @tweet.valid?
+      expect(@tweet.errors.full_messages).to include('User must exist')
+    end
+  end  
 end
