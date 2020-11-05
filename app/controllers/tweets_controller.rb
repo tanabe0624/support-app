bbe1a43baef2bc137790fd9dcf26935e3f6035db
tanabe0624 <ryo.tnb.bn@gmail.com>
2020-11-05 14:51:37 +0900

    投稿に関するアクションの処理の指定

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,23 @@
 class TweetsController < ApplicationController
   def index
   end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:title, :text, :category_id).merge(user_id: current_user.id)
+  end
 end
