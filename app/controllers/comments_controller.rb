ec732e25393d97f40cb4fdd726de4880ab10f5bb
tanabe0624 <ryo.tnb.bn@gmail.com>
2020-11-09 13:09:46 +0900

    Create comments_controller.rb

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,24 @@
+class CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to tweet_path(@comment.tweet)
+    else
+      @tweet = @comment.tweet
+      @comments = @tweet.comment
+      render "tweets/show"
+    end
+  end
+
+  def destroy
+    @comment = Comment.find_by(id: params[:id], tweet_id: params[:tweet_id])
+    @comment.destroy
+    redirect_to tweet_path(@comment.tweet)
+  end
+
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, tweet_id: params[:tweet_id])
+  end
+end
