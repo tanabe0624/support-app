611b2d2b3d1e9d3dd0f0cb102f2f4778fab6fad6
tanabe0624 <ryo.tnb.bn@gmail.com>
2020-11-17 19:02:36 +0900

    ER図挿入

--- README.md
+++ README.md
@@ -60,6 +60,10 @@
 
 # テーブル設計
 
+## EQ図
+![ER図](https://i.gyazo.com/60918799be51158891e0d0aeeb176144.png)
+
+
 ## users テーブル
 
 | Column        | Type    | Options     |
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @nickname = @user.nickname
-    @tweets = @user.tweets
+    @tweets = @user.tweets.order('created_at DESC')
   end
 end
