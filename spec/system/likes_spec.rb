f4bc81469126a27b2fc263331a90aef4f716fc68
tanabe0624 <ryo.tnb.bn@gmail.com>
2020-11-20 14:03:01 +0900

    いいね機能　結合テスト

---
+++ spec/system/likes_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe "Likes", type: :system do
+  before do
+    @user = FactoryBot.create(:user)
+    @tweet = FactoryBot.create(:tweet)
+  end
+
+  it 'ログインしたユーザーはツイート詳細ページでいいねできる' do
+    # ログインする
+    sign_in(@user)
+    # ツイート詳細ページに遷移する
+    visit tweet_path(@tweet)
+    # 投稿にいいねする
+    click_on('♡')
+    # いいねすると、likeモデルのカウントが1上がることを確認する
+    expect{
+      click_on('♡')
+    }.to change { Like.count }.by(1)
+    # 詳細ページ上に先ほどのコメント内容が含まれていることを確認する
+    expect(page).to have_content(@tweet.likes.count)
+  end
+end
